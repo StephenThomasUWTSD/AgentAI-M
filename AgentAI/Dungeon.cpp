3421b3e7d1790e82ef795f67d9634596de07b92a
StephenThomasUWTSD <stephen.skarzynski@googlemail.com>
2016-03-28 14:38:10 +0100

    primary commit

---
+++ AgentAI/Dungeon.cpp
@@ -0,0 +1,17 @@
+#include "Dungeon.h"
+
+//construct
+DungeonClass::DungeonClass()
+{
+}
+
+//destruct
+DungeonClass::~DungeonClass()
+{
+}
+
+//file in/out. for reading the dungeon from file(may break into its own class depending on how development goes
+void DungeonClass::fileIO()
+{
+
+}
---
+++ AgentAI/Dungeon.h
@@ -0,0 +1,10 @@
+
+class DungeonClass
+{
+public:
+	DungeonClass();
+	~DungeonClass();
+	
+	//file in/out. for reading the dungeon from file
+	void DungeonClass::fileIO();
+};
